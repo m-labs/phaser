7ba398211970de6aadf0e33c118f4463d229c9c1
Robert Jordens <jordens@gmail.com>
2016-07-30 13:13:52 +0200

    test ku105

---
+++ ku105.py
@@ -0,0 +1,28 @@
+from migen.build.generic_platform import *
+from migen.build.xilinx import XilinxPlatform, VivadoProgrammer
+
+
+_io = [
+    ("user_led", 0, Pins("AP8"), IOStandard("LVCMOS18")),
+
+    ("user_btn_c", 0, Pins("AE10"), IOStandard("LVCMOS18")),
+
+    ("clk125", 0,
+        Subsignal("p", Pins("G10"), IOStandard("LVDS")),
+        Subsignal("n", Pins("F10"), IOStandard("LVDS"))
+    ),
+]
+
+
+_connectors = [
+]
+
+
+class Platform(XilinxPlatform):
+    default_clk_name = "clk125"
+    default_clk_period = 8.
+
+    def __init__(self):
+        XilinxPlatform.__init__(self, "xcku040-ffva1156e-2", _io, _connectors)
+        self.toolchain.bitstream_commands = ["set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]"]
+        self.toolchain.additional_commands = ["write_cfgmem -force -format bin -interface spix4 -size 16 -loadbit \"up 0x0 {build_name}.bit\" -file {build_name}.bin"]
---
+++ test_ku105.py
@@ -0,0 +1,36 @@
+from migen import *
+from migen.build.generic_platform import *
+
+from sawg import Channel
+import ku105
+
+
+class Top(Module):
+    def __init__(self, platform):
+        width = 16
+        chs = [Channel(width, parallelism=8) for i in range(8)]
+        self.submodules += chs
+        # wire up q exchange
+        for i in range(0, len(chs), 2):
+            chs[i].connect_q(chs[i + 1])
+            chs[i + 1].connect_q(chs[i])
+
+        # just take random data from a sr to prevent folding
+        inp = platform.request("user_btn_c")
+        dat = Cat([[_.stb, _.payload.flatten()] for ch in chs for _ in ch.i])
+        sr = Signal(len(dat))
+        self.sync += sr.eq(Cat(inp, sr)), dat.eq(sr)
+
+        # add all outputs together to prevent folding
+        o = Signal((width, True))
+        for ch in chs:
+            for oi in ch.o:
+                o0, o = o, Signal.like(o)
+                self.sync += o.eq(o0 + oi)
+        self.sync += platform.request("user_led").eq(o[-1])
+
+
+if __name__ == "__main__":
+    platform = ku105.Platform()
+    top = Top(platform)
+    platform.build(top)
